b970c3ede864e0aa14b79018d6d25c82ced1999e
Jeet Biswas <125785193+Jeetbiswas23@users.noreply.github.com>
2024-02-22 12:27:33 +0530

    Update singlenumber.java

--- singlenumber.java
+++ singlenumber.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int count = 0; // Reset count for each iteration of i
             for (int j = 0; j < nums.length; j++) {
-                if (i != j && nums[i] == nums[j]) {
+                if (i != j && nums[i] == nums[j]) {// Here, i != j ensures that we're not comparing the same element with itself
                     count++;
                 }
             }
